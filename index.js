c22ecfd9aa4d3cc932ac5f27ac85d3c3a5c2f06f
Valdilene <valdilene.martins@gmail.comgit config --global user.email valdilene.martins@gmail.comgit config --global user.email valdilene.martins@gmail.com>
2020-01-28 16:09:15 -0300

    chore: Cria rotas

--- index.js
+++ index.js
@@ -22,4 +22,54 @@ function checkIdExist(req, res, next){
     return next()
 }
 
+//Routes
+
+//List all projects
+server.get('/projects', (req, res) =>{
+    return res.json(projects)
+})
+
+//List project according to id
+server.get('/projects/:id', checkIdExist, (req, res) =>{
+    const {id} = req.params
+    const project = projects.find(project => project.id === id)
+    return res.json(project)
+})
+
+server.post('/projects', (req, res) =>{
+    const {id, title, tasks} = req.body
+    const project = {            //Create project object
+        id : id,
+        title: title,
+        tasks: []
+    }
+    projects.push(project)       //Add project object in array projects
+    return res.json('Project created successfuly')
+})
+
+server.post('/projects/:id/tasks', checkIdExist, (req, res) =>{
+    const {id} = req.params
+    const {title} = req.body
+    const project = projects.find(project => project.id === id)
+    project.tasks.push(title)
+    return res.json('Task created successfuly')
+})
+
+server.put('/projects/:id', checkIdExist, (req, res) =>{
+    const {id} = req.params
+    const {title} = req.body
+    const project = projects.find(project => project.id === id)
+    project.title = title
+    return res.json(project)
+   
+})
+
+server.delete('/projects/:id', checkIdExist, (req, res) =>{
+    const {id} = req.params
+    const projectIndex = projects.findIndex(project => project.id === id)
+    projects.splice(projectIndex, 1)
+    return res.json('Project deleted successfuly')
+})
+
+
 server.listen(3000)
