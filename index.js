5b4945657d65b6ff9244edef3a67dd36b230f732
Valdilene <valdilene.martins@gmail.comgit config --global user.email valdilene.martins@gmail.comgit config --global user.email valdilene.martins@gmail.com>
2020-01-28 15:22:12 -0300

    chore: Cria middlewares

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ var count = 0
 
 //Middlewares
 
+server.use((req, res, next) =>{
+    count++
+    console.log(count + ' ' + 'requests made')
+    return next()
+})
+
+function checkIdExist(req, res, next){
+    const {id} = req.params
+    const project = projects.find(project => project.id === id) 
+    if(!project){
+        res.status(400).json('Project does not exists')
+    }
+    return next()
+}
+
 server.listen(3000)
